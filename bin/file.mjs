e61c3c9a8c657d89d7207d0e3b45309627d68b3c
lennygir <>
2022-02-28 20:34:39 +0100

    feat: git hook pre-commit

--- bin/file.mjs
+++ bin/file.mjs
@@ -1,35 +1,13 @@
+import { spawn } from 'child_process';
 import fs from 'fs';
+import { replaceInBuffer } from '../utils/buffer.mjs';
 
 const REPLACER = {
     'Xxxxx': (text) => text[0].toUpperCase() + text.slice(1).toLowerCase(),
     'xxxxx': (text) => text.toLowerCase()
 };
 
-// Buffer splice
-// If <length> is negative --> remove |length| items at <index>
-// Else add <data> |length| times at <index>
-function splice(buff, index, length, data) {
-    if(length > 0) {
-        return Buffer.concat([buff.slice(0, index), buff.slice(index + length, buff.length)]);
-    }
-    return Buffer.concat([buff.slice(0, index), Buffer.alloc(-length, data), buff.slice(index, buff.length)]);
-}
-
-function replaceInBuffer(buff, toReplace, substitute) {
-    let index;
-    while((index = buff.indexOf(toReplace)) !== -1) {
-        // Add or remove space according to substitude length
-        buff = splice(buff, index, toReplace.length - substitute.length, 0);
-        // Change letters
-        for(let i = 0; i < substitute.length; ++i) {
-            buff[index + i] = substitute[i].charCodeAt(0);
-        }
-    }
-    return buff;
-}
-
 function generate(endPath, templatePath, type, name) {
-    console.log(endPath);
     fs.readFile(`${templatePath}/../../templates/${type}.txt`, (error, data) => {
         if(error) {
             if(error.errno === -4058) {
@@ -38,11 +16,9 @@ function generate(endPath, templatePath, type, name) {
                 throw error;
             }
         } 
-        // Replace dynamic data (names)
         for(const toReplace in REPLACER) {
             data = replaceInBuffer(data, toReplace, REPLACER[toReplace](name));
         }
-        // ...
         fs.writeFile (`${endPath}/${name}${type[0].toUpperCase() + type.slice(1).toLowerCase()}.ts`, data, (error) => {
             if(error) {
                 throw error;
@@ -52,4 +28,11 @@ function generate(endPath, templatePath, type, name) {
     });
 }
 
-export { generate };
+function init() {
+    spawn('npm', ['init'], {
+        shell: true,
+        stdio: 'inherit'
+    });
+}
+
+export { generate, init };
